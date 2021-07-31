27d7d61d42a64bbf3c3746587c62aaf70e859374
JackJackHuo <86769801+JackJackHuo@users.noreply.github.com>
2021-07-31 13:58:00 +0800

    Add files via upload

--- PRG/SideProject_RPG.js
+++ PRG/SideProject_RPG.js
@@ -7,77 +7,56 @@ class CreateCharactor{
       
     }
     
-    attack(){
-        let damage = Math.floor(Math.random() * 50 )+1
-        return damage
+    attack(enemy){
+      let result = ''  
+      let damage = Math.floor(Math.random() * 50 )+1
+      enemy.hp = enemy.hp - damage
+      console.log(`${this.name} attack ${enemy.name} ${enemy.name} loses ${damage} points`)
+      if(enemy.hp > 0){
+        result = `${enemy.name} still alive (HP = ${enemy.hp})`
+      }else if(enemy.hp < 0){
+        result = `${enemy.name} is dead`
+      }  
+      return result
     }
 
-    cure(mp){
-        
-        if(mp>30){
-          return 15
+    cure(defaultHpGain){
+      let result = ''
+      let remedy
+      if(this.hp > 0){
+        if(this.mp >= 30){
+          remedy = defaultHpGain
+          this.hp = this.hp + defaultHpGain
+          this.mp = this.mp - defaultHpGain*2
+        }else if(this.mp < 30){
+          remedy = Math.floor(this.mp/2)
+          this.hp = this.hp + remedy
+          this.mp = this.mp - 2* remedy
         }
-        if(mp <= 30){
-          return Math.floor(mp/2)
-          
-        }
-        
+        result = `${this.name} HP recovered ${remedy} (HP = ${this.hp} MP = ${this.mp})\n====== CHANGE SIDE ======`         
+      }else{
+        result = `GAME OVER`
+      }       
+      return result
     }
 }
 
-  
-  const magician = new CreateCharactor('Magician' , 30 , 100)
-  const warrior = new CreateCharactor('Warrior' , 100 , 30)
+console.log('====== CREATE PLAYERS ======')
+  const magician = new CreateCharactor('Magician' , 50 , 100)
+  const warrior = new CreateCharactor('Warrior' , 100 , 59  )
 
   console.log(warrior)
   console.log(magician)
+
+  console.log('====== START FIGHT ======')
   while(warrior.hp > 0 && magician.hp > 0){
-    warriorAttack()
-    magicianAttack()
-  }
-
-  console.log(warrior)
-  console.log(magician)
-
-function warriorAttack(){   
-        let damage = warrior.attack()
-        let magicianHP = magician.hp
-        magician.hp = magician.hp - damage
-        console.log(`======${warrior.name} attack======` )
-        console.log(`${warrior.name} attack ${damage}` )
-        console.log(`${magician.name} remaining HP = ${magician.hp} (${magicianHP} - ${damage})` )
-        if(magician.hp > 0){
-            let remedy = magician.cure(magician.mp)
-            console.log(`${magician.name} MP = ${magician.mp}` )
-            magician.hp = magician.hp + remedy
-            console.log(`${magician.name} HP +  ${remedy} = ${magician.hp} HP` )
-            magician.mp = magician.mp - (remedy *2)
-        }else{
-          console.log(`==========RESULT==========` )
-          console.log(`${magician.name} KILLED!!!` )
-          console.log(`${warrior.name} WINS!!!` )
-        }
-  }
-  function magicianAttack(){
+    console.log(warrior.attack(magician))
+    console.log(magician.cure(15))
     if(magician.hp > 0){
-        let damage = magician.attack()
-        let warriorHP = warrior.hp
-        warrior.hp = warrior.hp - damage
-        console.log(`======${magician.name} attack======` )
-        console.log(`${magician.name} attack ${damage}` )
-        console.log(`${warrior.name} remaining HP = ${warrior.hp} (${warriorHP} - ${damage})` )
-        if(warrior.hp > 0){
-          let remedy = warrior.cure(warrior.mp)
-          warrior.hp = warrior.hp + remedy
-          console.log(`${warrior.name} MP = ${warrior.mp}` )          
-          console.log(`${warrior.name} HP +  ${remedy} = ${warrior.hp} HP` )
-          warrior.mp = warrior.mp - (remedy*2)
-        }else{
-          console.log(`==========RESULT==========` )
-          console.log(`${warrior.name} KILLED!!!` )      
-          console.log(`${magician.name} WINS!!!` )
-        }
-    }
+      console.log(magician.attack(warrior))
+      console.log(warrior.cure(15))
+      } 
   }
-  
- 
+
+
+
